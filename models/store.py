8cf6d577a771dc0ec7157eb5821235c4ce1de716
Mael <maelvittorio@gmail.com>
2021-03-23 11:54:34 +0100

    created a simple flask app

---
+++ models/store.py
@@ -0,0 +1,30 @@
+from db import db
+
+class StoreModel(db.Model) :#extend model
+
+    __tablename__ = "stores"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80))
+    items = db.relationship("ItemModel", lazy="dynamic")#is a query builder, not a store of all items
+
+
+
+    def __init__ (self, name):
+        self.name = name
+
+    def json(self):
+        return {"name": self.name, "items": [item.json() for item in self.items.all()]}#all is used for the lazy 
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first() #SELECT * FROM items WHERE name=name  LIMIT 1
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+    
